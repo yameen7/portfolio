9ae7f0affee1defdc422d45f59ad56a7f244a5f3
Mo Yameen Macchhar <myameen033@gmail.com>
2025-05-05 22:53:55 +0530

    project options updated

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -3,6 +3,7 @@
 import { motion } from "framer-motion"
 import { Code, ExternalLink } from "lucide-react"
 import { Button } from "@/components/ui/button"
+import Link from "next/link"
 
 export default function Projects() {
   const projects = [
@@ -11,12 +12,19 @@ export default function Projects() {
       description:
         "Developed features that allows user to track your daily expenses and suggest the daily spend limit based on users revenue cycle.",
       tools: "Reactjs, nodejs, mongodb, shadcn(UI library)",
+      link : "https://moneytracker-sigma.vercel.app"
     },
     {
       title: "Spectrom",
       description:
         "Displays information about all events such as rules, fees, place, and type of participation (group or single) and allows the user to register for the events.",
       tools: "PHP, Javascript, Jquery, Mysql and bootstrap.",
+    },
+    {
+      title: "Time Logger",
+      description:
+        "Keka Time Logger is a smart Chrome extension that gives you clear and detailed insights into your daily logs and time usage. It helps you understand how your time is spent across workdays — empowering you to track productivity effortlessly.",
+      link: "https://chromewebstore.google.com/detail/keka-time-logger/dpdfhijiikhcbpkeciiknfffnapajcid/reviews?hl=en",
     },
   ]
 
@@ -45,15 +53,15 @@ export default function Projects() {
               <div className="p-6">
                 <div className="flex items-center justify-between mb-4">
                   <h3 className="text-xl font-bold">{project.title}</h3>
-                  <Button variant="ghost" size="icon" className="opacity-0 group-hover:opacity-100 transition-opacity">
-                    <ExternalLink className="h-4 w-4" />
-                  </Button>
+                  {project.link && <Button variant="ghost" size="icon" className="opacity-0 group-hover:opacity-100 transition-opacity">
+                    <Link href={project.link} target="_blank"><ExternalLink className="h-4 w-4"></ExternalLink></Link>
+                  </Button>}
                 </div>
                 <p className="text-muted-foreground mb-4">{project.description}</p>
-                <div className="flex items-center text-sm text-muted-foreground">
+                {project.tools && <div className="flex items-center text-sm text-muted-foreground">
                   <span className="font-medium">Tech Stack:</span>
                   <span className="ml-2">{project.tools}</span>
-                </div>
+                </div>}
               </div>
             </motion.div>
           ))}
